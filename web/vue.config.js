d8ffd8faff7b5f7e09d31fac0826ddb456a4ef2d
黄健宸 <yilang0989@163.com>
2023-01-10 16:33:16 +0800

    开发环境代理配置

--- web/vue.config.js
+++ web/vue.config.js
@@ -3,13 +3,13 @@ module.exports = {
     devServer: {
       historyApiFallback: true,
       // when in dev env:
-      // proxy: {
-      //   '^/api': {
-      //     target: 'http://localhost:3000/',
-      //     ws: true,
-      //     changeOrigin: true,
-      //   },
-      // },
+      proxy: {
+        '^/api': {
+          target: 'http://localhost:3000/',
+          ws: true,
+          changeOrigin: true,
+        },
+      },
     },
   },
   chainWebpack: (config) => {
